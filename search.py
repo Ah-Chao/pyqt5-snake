e5611c27f1659041f4bf234edfa194c832a52141
Ah-Chao <278432384@qq.com>
2019-05-15 14:16:43 +0800

    Now it has three search algorithms that implement get the move from games

--- search.py
+++ search.py
@@ -19,12 +19,6 @@ actions = [[1, 3, 4],
            [1, 2, 3],
            [1, 2, 4]]
 
-'''
-测试结果，该函数只会进行坐标的比较，然后就进行动作的发生了， 
-丝毫不考虑这个东西会不会导致游戏的结束，所以下一步应该考虑，
-如何保证这个游戏不会发生这种情况
-'''
-
 
 def naive_get_snake_move(head,food):
     ''''
@@ -150,8 +144,6 @@ def greedy_first_search(snake,direction,food):
 
         tmp_node_list = []
         for one_node in tmp_node.children:
-            # print(one_node.snake_state)
-            # print(one_node.snake_state,food)
             if one_node.node_success(food):
                 return get_move_sequence(one_node)
             if not one_node.node_safe():
@@ -167,7 +159,7 @@ def greedy_first_search(snake,direction,food):
                                         food[0]),
                                        one_node))
 
-        distance_node_list.sort(key = lambda x:x[0])
+        distance_node_list.sort(key = lambda x:x[0],reverse = True)
         for one_node in distance_node_list:
             node_queue.insert(0,one_node[1])
     return None
